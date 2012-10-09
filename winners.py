131fe983be33f182af8a566117f22420ba58b8fb
Alexander Zaycev (laptop) <thebestzorro@ya.ru>
2012-10-09 19:46:45 +0400

    change files structure

---
+++ winners.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+class ClassName(object):
+    """docstring for ClassName"""
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.arg = arg
